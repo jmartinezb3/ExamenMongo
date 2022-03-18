8a6215883d223b2877058a69d43b737524699eb9
Joshua Martinez <jmartinezb3@est.ups.edu.ec>
2022-03-17 22:06:31 -0500

    final final

--- red/rutas.js
+++ red/rutas.js
@@ -1,12 +1,8 @@
-const factura = require('../componentes/factura/interfaz')
-const cliente = require('../componentes/cliente/interfaz')
-const producto = require('../componentes/producto/interfaz')
+
 const proyecto = require('../componentes/proyecto/interfaz')
 
 const rutas = function(servidor) {
-    servidor.use('/factura', factura)
-    servidor.use('/cliente', cliente)
-    servidor.use('/producto', producto)
+    
     servidor.use('/proyecto', proyecto)
 }
 
